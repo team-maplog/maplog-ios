23e73b56f94f196b8a53cdeae9bb3902bc94303c
Chaerim Han <96909637+cozyrim@users.noreply.github.com>
2026-07-19 05:01:45 +0900

    feat: add initial implementation of SignupView and related auth services

---
+++ Maplog/Auth/SignupView.swift
@@ -0,0 +1,14 @@
+//
+//  SignupView.swift
+//  Maplog
+//
+//  Created by 한채림 on 7/19/26.
+//
+
+import SwiftUI
+
+struct SignupView: View {
+    var body: some View {
+        Text("회원가입")
+    }
+}
---
+++ Maplog/Services/APIClient.swift
@@ -0,0 +1,7 @@
+//
+//  APIClienttled.swift
+//  Maplog
+//
+//  Created by 한채림 on 7/18/26.
+//
+
